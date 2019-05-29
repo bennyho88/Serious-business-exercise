d6728944b9d550eace7de28f9d1362306cd278d2
Benny Ho <benny.ho2530@gmail.com>
2019-05-29 12:14:19 +0200

    added 2 columns, time and date

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+document.getElementById("btn").addEventListener("click", time);
+
+function time() { 
+
+var today = new Date();
+document.getElementById('datetime').innerHTML = today;
+
+}
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date + ' ' + time;
+
+
+/*
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[today.getDay()];
+*/
